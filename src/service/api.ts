d3368ad511704e2af3a59dfbb14ea28e896cc16e
Vinicius Santana <viniciussantanaleao@gmail.com>
2022-06-15 16:38:36 -0300

    put correct adress api

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'https://bytecode-front.herokuapp.com/api/'
+  baseURL: 'https://bytecode-api.herokuapp.com/api'
 })
 
 export const HttpPrivate = axios.create({
-  baseURL: 'https://bytecode-front.herokuapp.com/api/',
+  baseURL: 'https://bytecode-api.herokuapp.com/api',
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true
 })
