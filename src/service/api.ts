62de2d5d7d5af8d5ab7e13e0c8553927dd7f70ef
Vinicius Santana <viniciussantanaleao@gmail.com>
2022-06-15 16:22:59 -0300

    add api in path

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'https://bytecode-front.herokuapp.com/'
+  baseURL: 'https://bytecode-front.herokuapp.com/api/'
 })
 
 export const HttpPrivate = axios.create({
